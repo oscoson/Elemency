5615000d732ed305c370d34f3b3d439303fa9fa5
OscoSon <oscarsani007@gmail.com>
2022-06-26 23:38:37 -0400

    Fire Slime wall interaction
    - Sprite now switches direction upon hitting a wall

--- Elemency/Assets/Scripts/FireSlime.cs
+++ Elemency/Assets/Scripts/FireSlime.cs
@@ -8,12 +8,33 @@ public class FireSlime : MonoBehaviour
     public float slimeHealth;
     public float slimeDamage;
     public float slimeWalkSpeed;
+    private Rigidbody2D slimeRB;
 
 
     private void Start()
     {
+        slimeRB = GetComponent<Rigidbody2D>();
         slimeHealth = slimeSO.health;
         slimeDamage = slimeSO.damage;
         slimeWalkSpeed = slimeSO.walkSpeed;
+    }
+
+    private void Update()
+    {
+        slimeRB.velocity = new Vector2(slimeWalkSpeed, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Platforms")
+        {
+            slimeWalkSpeed = -slimeWalkSpeed;
+            FlipSprite();
+        }
+    }
+
+    private void FlipSprite()
+    {
+        transform.localScale = new Vector2(-(transform.localScale.x), 0.2257035f);
     }
 }
