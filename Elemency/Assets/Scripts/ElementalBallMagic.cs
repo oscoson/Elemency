054b5707aed9696b8d11101ee80f345a75fb9cbd
OscoSon <oscarsani007@gmail.com>
2022-07-26 18:00:15 -0400

    Elemental Ball Additions/Changes
    - FireBallMagic script changed to ElementalBallMagic
    - Added in Water Ball prefab along with the remaining elemental tags + Water Ball  animation + Water particles
    - Water Ball ScriptableObject added

--- Elemency/Assets/Scripts/ElementalBallMagic.cs
+++ Elemency/Assets/Scripts/ElementalBallMagic.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireBallMagic : MonoBehaviour
+public class ElementalBallMagic : MonoBehaviour
 {
     [Header("Attributes")]
     public ElementalBall elementalBallSO;
