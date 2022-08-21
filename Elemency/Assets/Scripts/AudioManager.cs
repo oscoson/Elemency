b898a585b2889a0cd4247567aec590ef2312d3f5
OscoSon <oscarsani007@gmail.com>
2022-08-20 23:47:14 -0400

    Sound Scripts Created
    - Created Sound Script for displaying and configuring attributes of the Sound Effects/Music in the inspector
    - Created AudioManager to allow sounds and music to be played in game, as well as to house methods that allow for audio calls to happen in other scripts

---
+++ Elemency/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+   public Sound[] sounds;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = s.mixer;
+        }
+        
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+
+    public void StopLoop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Stop();
+    }
+
+    public void Pause(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Pause();
+    }
+    public void UnPause(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.UnPause();
+    }
+}
